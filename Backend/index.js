695e1fac8d88b4bfdd73ba8469f8c9aee895334b
suryagit-31 <suryadammalapa@gmail.com>
2025-09-12 12:58:19 +0530

    version 1

--- Backend/index.js
+++ Backend/index.js
@@ -16,10 +16,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://dental-care-nu-seven.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // Routes
